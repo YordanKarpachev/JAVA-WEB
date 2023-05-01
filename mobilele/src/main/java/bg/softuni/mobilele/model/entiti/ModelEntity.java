9b8fb67b3a647ea790d91a00fe5b52ceb7639dc9
YordanKarpachev <yordkarp@icloud.com>
2023-05-01 19:16:24 +0200

    Model added.

--- mobilele/src/main/java/bg/softuni/mobilele/model/entiti/ModelEntity.java
+++ mobilele/src/main/java/bg/softuni/mobilele/model/entiti/ModelEntity.java
@@ -2,18 +2,18 @@ package bg.softuni.mobilele.model.entiti;
 
 import bg.softuni.mobilele.model.entiti.enums.CategoryEnums;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
 
-@Entity(name = "models")
+@Entity
+@Table(name = "models")
 public class ModelEntity extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
-   private CategoryEnums category;
+    private CategoryEnums category;
 
-    @Column(name = "image_url")
+    @Column(nullable = false)
+    private String name;
+    @Column(name = "image_url", nullable = false)
     private String imageUrl;
 
     @Column(name = "start_year")
@@ -22,5 +22,67 @@ public class ModelEntity extends BaseEntity {
     @Column(name = "end_year")
     private int endYear;
 
-    private String brand;
+    @ManyToOne
+    private BrandEntity brand;
+
+
+    public CategoryEnums getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnums category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getStartYear() {
+        return startYear;
+    }
+
+    public void setStartYear(int startYear) {
+        this.startYear = startYear;
+    }
+
+    public int getEndYear() {
+        return endYear;
+    }
+
+    public void setEndYear(int endYear) {
+        this.endYear = endYear;
+    }
+
+    public BrandEntity getBrand() {
+        return brand;
+    }
+
+    public void setBrand(BrandEntity brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "ModelEntity{" +
+                "category=" + category +
+                ", name='" + name + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", startYear=" + startYear +
+                ", endYear=" + endYear +
+                ", brand=" + brand != null ? brand.getName() : ""+
+                '}';
+    }
 }
